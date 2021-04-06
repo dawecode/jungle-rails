662fd7b1423106e85aa3502e9a26e9aeeb6462d9
Catherine Dawe <dawe.cm@gmail.com>
2021-04-05 20:08:45 -0400

    add test for users

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,12 +6,19 @@ RSpec.describe Product, type: :model do
 
 
  describe 'Validations' do 
+    it "saves successfully when all fields are present" do
+      @categories = Category.new(:name => "Vintage Clothing")
+      @product = Product.new(:name => "Sun dress",  :price => 9000, :quantity => 1, :category =>@categories)
+      expect(@product).to be_valid 
+    end 
+
     it "validates the presence of name" do 
       @categories = Category.new(:name => "Vintage Clothing")
       @product = Product.new(:name => nil,  :price => 9000, :quantity => 1, :category =>@categories)
       expect(@product).not_to be_valid 
       expect(@product.errors.full_messages[0]).to eq("Name can't be blank")
     end 
+
     it "validates the presence of price_cents and price" do 
       @categories = Category.new(:name => "Vintage Clothing")
       @product = Product.new(:name => "Sunflower dress", :price => nil, :price_cents =>nil, :quantity => 1, :category =>@categories)
@@ -20,13 +27,15 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages[1]).to eq("Price is not a number")
       expect(@product.errors.full_messages[2]).to eq("Price can't be blank")
     end 
+
     it "validates the presence of quantity" do 
       @categories = Category.new(:name => "Vintage Clothing")
       @product = Product.new(:name => "Sunflower dress", :price => 9000, :quantity =>nil, :category =>@categories)
       expect(@product).not_to be_valid 
       expect(@product.errors.full_messages[0]).to eq("Quantity can't be blank")
     end 
-    it "validates the presence of categorie" do 
+
+    it "validates the presence of category" do 
       @categories = Category.new(:name => "Vintage Clothing")
       @product = Product.new(:name => "Sunflower dress", :price => 9000, :quantity =>1, :category =>nil )
       expect(@product).not_to be_valid 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,54 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
- describe 'Validations' do 
-  it "validates the presence of  first name" do 
-    @user = User.new(:first_name => nil, :last_name => "test", :email => "test@test.com", :password =>"test")
-    expect(@user).not_to be_valid 
-    expect(@user.errors.full_messages[0]).to eq("First name can't be blank")
-  end 
+  describe 'Validations' do 
+    it "validates the presence of  first name" do 
+      @user = User.new(:first_name => nil, :last_name => "test", :email => "test@test.com", :password =>"test", :password_confirmation => "test")
+      expect(@user).not_to be_valid 
+      expect(@user.errors.full_messages[0]).to eq("First name can't be blank")
+    end 
 
-  it "validates the presence of last name" do 
-    @user = User.new(:first_name => "test", :last_name => nil,  :email => "test@test.com", :password_digest => "test")
-    expect(@user).not_to be_valid 
-    expect(@user.errors.full_messages[0]).to eq("Last name can't be blank")
-  end 
+    it "validates the presence of last name" do 
+      @user = User.new(:first_name => "test", :last_name => nil,  :email => "test@test.com", :password => "test",:password_confirmation => "test")
+      expect(@user).not_to be_valid 
+      expect(@user.errors.full_messages[0]).to eq("Last name can't be blank")
+    end 
+
+    it "validates the presence of email" do 
+      @user = User.new(:first_name => "test", :last_name => "2test",  :email => nil, :password => "test12", :password_confirmation => "test12")
+      expect(@user).not_to be_valid 
+      expect(@user.errors.full_messages[0]).to eq("Email can't be blank")
+    end 
+
+    it "should prevent user creation if the passwords don't match" do
+      @user = User.new(:first_name => "test", :last_name => "2test",  :email => "test@test.com" , :password => "test12", :password_confirmation => "2test1" )
+      expect(@user).not_to be_valid 
+      expect(@user.errors.full_messages[0]).to eq("Password confirmation doesn't match Password")
+    end
+
+    it "password should have minimum 3 charaters " do 
+      @user = User.new(:first_name => "test", :last_name => "2test",  :email => "test@test.com" , :password => "test", :password_confirmation => "test" )
+      expect(@user).not_to be_valid 
+      expect(@user.errors.full_messages[0]).to eq("Password is too short (minimum is 6 characters)")
+    end
+
+
+    it "emails should be unique case sensitive " do 
+      @user = User.new(:first_name => "test", :last_name => "2test",  :email => "test@test.com" , :password => "test12", :password_confirmation => "test12" )
+      @user.save
+      @user2 =User.new(:first_name => "2test", :last_name => "test",  :email => "test@test.COM" , :password => "2test1", :password_confirmation => "2test1" )
+      expect(@user2).not_to be_valid 
+      expect(@user2.errors.full_messages[0]).to eq("Email has already been taken")
+    end
+
+    it "emails should be unique NOT case sensitive " do 
+      @user = User.new(:first_name => "test", :last_name => "2test",  :email => "test@test.com" , :password => "test12", :password_confirmation => "test12" )
+      @user.save
+      @user2 =User.new(:first_name => "2test", :last_name => "test",  :email => "test@test.com" , :password => "2test1", :password_confirmation => "2test1" )
+      expect(@user2).not_to be_valid 
+      expect(@user2.errors.full_messages[0]).to eq("Email has already been taken")
+    end
+
 
 
   end
